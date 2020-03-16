c0b90460a161f7353315c6ddd9ac109f2d8b22c7
Anton Zolotov <antonzolotov@outlook.com>
2020-03-16 20:46:51 +0500

    All styles has been extracted from templates to separated scss files compiled into one styles.scss

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -11,20 +11,3 @@
         >
     </router-link>
 </template>
-
-<style lang="scss">
-@import "../assets/app.scss";
-.logo {
-    width: 4em !important;
-    line-height: 0;
-    margin-right: 0 !important;
-    transition: all 0.2s ease-out;
-    &:hover {
-        filter: sepia(1) brightness(90%) hue-rotate(150deg) ;
-    }
-    .logo-img {
-        width: 3.5em;
-    }
-}
-
-</style>
