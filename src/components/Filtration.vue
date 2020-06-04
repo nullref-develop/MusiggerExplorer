674929d979b4b9d5a82a1c18566eee8c85081c03
Anton Zolotov <antonzolotov@outlook.com>
2020-06-04 19:46:56 +0500

    Redesigned filter section. A lot of changes in voice search.

--- src/components/Filtration.vue
+++ src/components/Filtration.vue
@@ -2,77 +2,54 @@
     <div class="filter">
         <transition name="slide-fade">
             <div v-if="showFilter" class="grid-x grid-margin-x" style="margin-bottom: 1em;">
-                <div class="filter-item large-9 medium-9 cell">
-                    <label>Search</label>
-                    <input
-                        v-model="titleQuery"
-                        type="text"
-                        class="finder-input margin-0"
-                        placeholder="Search for titles and artists"
-                        @change="filterInput"
-                        @keyup="filterInput"
-                    >
-                    <button
-                        v-if="SpeechRecognition.Available"
-                        class="button"
-                        type="button"
-                        role="button"
-                        style="border: 2px solid #f0f0f0;"
-                        @click="speechRecognise()"
-                    >
-                        Say it (beta)
-                    </button>
-                </div>
-                <div class="filter-item large-3 medium-3 cell">
-                    <label>Releases per page</label>
-                    <input
-                        v-model.number="perPage"
-                        type="number"
-                        class="finder-input"
-                        @change="filterInput"
-                        @keyup="filterInput"
-                    >
-                </div>
                 <div class="filter-item large-3 medium-6 cell">
-                    <label>Genres</label>
+                    <label>Genre</label>
                     <multiselect
                         v-model="selectedGenres"
                         :options="genres"
                         :multiple="true"
-                        placeholder="Select genres"
+                        placeholder="Select genre"
                         @input="filterInput"
                     />
                 </div>
                 <div class="filter-item large-3 medium-6 cell">
-                    <label>Types</label>
-                    <multiselect
-                        v-model="selectedTypes"
-                        :options="types"
-                        :multiple="true"
-                        placeholder="Select release types"
-                        @input="filterInput"
-                    />
-                </div>
-                <div class="filter-item large-3 medium-6 cell">
-                    <label>Labels</label>
+                    <label>Label</label>
                     <multiselect
                         v-model="selectedLabels"
                         :options="labels"
                         :multiple="true"
-                        placeholder="Select record labels"
+                        placeholder="Select record label"
                         @search-change="getLabels"
                         @close="filterInput"
                         @remove="filterInput"
                     />
                 </div>
-                <div class="filter-item large-3 medium-6 cell">
-                    <label>Votes</label>
+                <div class="filter-item large-2 medium-4 cell">
+                    <label>Release type</label>
+                    <multiselect
+                        v-model="selectedTypes"
+                        :options="types"
+                        :multiple="true"
+                        placeholder="Release type"
+                        @input="filterInput"
+                    />
+                </div>
+                <div class="filter-item large-2 medium-4 cell">
+                    <label>Number of votes</label>
                     <input
                         v-model.number="votes"
                         type="number"
                         class="finder-input"
-                        @change="filterInput"
-                        @keyup="filterInput"
+                        @input="filterInput"
+                    >
+                </div>
+                <div class="filter-item large-2 medium-4 cell">
+                    <label>Releases per page</label>
+                    <input
+                        v-model.number="perPage"
+                        type="number"
+                        class="finder-input"
+                        @input="filterInput"
                     >
                 </div>
             </div>
@@ -80,47 +57,50 @@
         <div class="grid-x grid-margin-x">
             <Logo />
             <div class="auto cell">
-                <transition name="fade">
-                    <div v-if="!showFilter" class="filter-mini">
-                        <span v-if="titleQuery">
-                            <icon name="search" scale="0.8" />
-                            {{ titleQuery }}&nbsp;&nbsp;
-                        </span>
-                        <span v-if="artistsQuery">
-                            <icon name="user" scale="0.8" />
-                            {{ artistsQuery }}&nbsp;&nbsp;
-                        </span>
-                        <span v-if="genresQuery" class="genres">
-                            <icon name="music" scale="0.8" />
-                            {{ genresQuery }}&nbsp;&nbsp;
-                        </span>
-                        <span v-if="typesQuery">
-                            <icon name="play" scale="0.8" />
-                            {{ typesQuery }}&nbsp;&nbsp;
-                        </span>
-                        <span v-if="labelsQuery">
-                            <icon name="tag" scale="0.8" />
-                            {{ labelsQuery }}&nbsp;&nbsp;
-                        </span>
-                        <span v-if="votes">
-                            <icon name="users" scale="0.8" />
-                            {{ votes }}&nbsp;&nbsp;
-                        </span>
-                    </div>
-                </transition>
+                <div class="filter-item auto cell grid-x">
+                    <input
+                        v-model="titleQuery"
+                        type="text"
+                        class="finder-input is-search margin-0 auto cell"
+                        :placeholder="SearchBar.Placeholder"
+                        @focus="searchChangeFocus"
+                        @blur="searchChangeFocus"
+                        @input="filterInput"
+                    >
+                    <button
+                        v-if="SpeechRecognition.Available"
+                        class="speech-button button shrink cell"
+                        type="button"
+                        role="button"
+                        alt="Voice search"
+                        aria-label="Voice search"
+                        @click="speechRecognise()"
+                    >
+                        <icon
+                            :name="SpeechRecognitionIcon"
+                            :pulse="SpeechRecognitionIconPulse"
+                            :spin="SpeechRecognitionIconSpin"
+                            :color="(SpeechRecognition.Status.Listening) ? 'red' : ''"
+                            scale="0.9"
+                        />
+                    </button>
+                </div>
             </div>
-            <div class="medium-4 small-8 cell">
-                <button
+            <div class="shrink cell">
+                <svg
+                    class="filter-button button burger"
                     :class="{ active: showFilter }"
-                    class="filter-button button"
-                    type="button"
+                    viewBox="0 0 100 100"
+                    width="40"
                     role="button"
                     alt="Show/hide filters panel"
                     aria-label="Show/hide filters panel"
                     @click="ShowFilter()"
                 >
-                    <icon name="chevron-down" scale="0.7" />
-                </button>
+                    <path class="line top" d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20" />
+                    <path class="line middle" d="m 70,50 h -40" />
+                    <path class="line bottom" d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
+                </svg>
                 <button
                     v-if="showFilter"
                     class="clearfilter-button filter-button button"
@@ -131,24 +111,7 @@
                     @click="clearFilter"
                 >
                     <icon name="trash" scale="0.8" />
-                    Clear filter
                 </button>
-                <!-- If no one filter is set -->
-                <span
-                    v-if="
-                        !artistsQuery &&
-                            !genresQuery &&
-                            !typesQuery &&
-                            !labelsQuery &&
-                            !votes &&
-                            !titleQuery &&
-                            !showFilter
-                    "
-                    class="filterinfotext"
-                >
-                    Push the button to show the filters
-                    <icon name="angle-double-right" scale="0.8" />
-                </span>
             </div>
         </div>
     </div>
@@ -229,8 +192,34 @@ export default {
             titleQuery: "",
             LabelLoaded: false,
             SpeechRecognition: {
-                Available: false
+                Available: false,
+                RecognitionAPI: null,
+                RecognitionEngine: null,
+                Status: {
+                    Ready: false,
+                    Listening: false,
+                    Analyzing: false
+                }
+            },
+            SearchBar: {
+                Focused: false,
+                Placeholder: "Search by artist/title"
             }
+        }
+    },
+    computed: {
+        SpeechRecognitionIcon() {
+            if (this.SpeechRecognition.Status.Listening) return "circle-notch"
+            if (this.SpeechRecognition.Status.Analyzing) return "spinner"
+            return "microphone"
+        },
+        SpeechRecognitionIconPulse() {
+            if (this.SpeechRecognition.Status.Analyzing) return true
+            return false
+        },
+        SpeechRecognitionIconSpin() {
+            if (this.SpeechRecognition.Status.Listening) return true
+            return false
         }
     },
     created() {
@@ -254,37 +243,45 @@ export default {
         this.perPage = this.perPageP
         // load full list of genres from web api
         this.getAllGenres()
+        // check speech recognition availability
         this.speechCheckAvailability()
+        // if params exist - show filter panel
+        if (this.genresQueryP || this.labelsQueryP || this.typesQueryP || this.artistsQuery || this.votes || this.perPage !== 24) {
+            this.showFilter = true
+        }
     },
     methods: {
-        // casting of variable types
         filterInput: debounce(
             // задержка поиска при пользовательском вводе
             function () {
-                this.currentPage = 1 // reset current page when filter change
-                this.typesQuery = this.selectedTypes != null
-                    ? this.selectedTypes.join()
-                    : null
-                this.labelsQuery = this.selectedLabels != null
-                    ? this.selectedLabels.join()
-                    : null
-                this.genresQuery = this.selectedGenres != null
-                    ? this.selectedGenres.join()
-                    : null
-                this.$emit(
-                    "filter-changed",
-                    this.currentPage,
-                    this.votes,
-                    this.perPage,
-                    this.genresQuery,
-                    this.labelsQuery,
-                    this.typesQuery,
-                    this.artistsQuery,
-                    this.titleQuery
-                )
+                this.filter()
             },
-            1000
+            1500
         ),
+        // casting of variable types
+        filter() {
+            this.currentPage = 1 // reset current page when filter change
+            this.typesQuery = this.selectedTypes != null
+                ? this.selectedTypes.join()
+                : null
+            this.labelsQuery = this.selectedLabels != null
+                ? this.selectedLabels.join()
+                : null
+            this.genresQuery = this.selectedGenres != null
+                ? this.selectedGenres.join()
+                : null
+            this.$emit(
+                "filter-changed",
+                this.currentPage,
+                this.votes,
+                this.perPage,
+                this.genresQuery,
+                this.labelsQuery,
+                this.typesQuery,
+                this.artistsQuery,
+                this.titleQuery
+            )
+        },
         clearFilter() {
             this.labelsQuery = ""
             this.genresQuery = ""
@@ -328,11 +325,22 @@ export default {
             this.showFilter = !this.showFilter
             if (this.labels.length === 0 && !this.LabelLoaded) this.getLabels()
         },
+        searchChangeFocus() {
+            if (this.SearchBar.Focused) {
+                this.SearchBar.Focused = false
+                this.SearchBar.Placeholder = "Search by artist/title"
+            }
+            else {
+                this.SearchBar.Focused = true
+                this.SearchBar.Placeholder = "Enter the name of the artist/release or click the button on the right and just say it"
+            }
+        },
         speechCheckAvailability() {
             if (("webkitSpeechRecognition" in window)) {
                 this.SpeechRecognition.Available = true
+                this.speechInitialize()
             }
-            else console.warn("Web Speech API is not supported by this browser.")
+            // else console.warn("Web Speech API is not supported by this browser.")
         },
         speechCapitalize(text) {
             const sentence = text.split(" ")
@@ -341,34 +349,48 @@ export default {
             }
             return sentence.join(" ")
         },
-        speechRecognise() {
+        speechInitialize() {
             /* eslint-disable no-use-before-define */
             /* eslint-disable no-undef */
-            const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-            const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-            const recognition = new SpeechRecognition()
+            this.SpeechRecognition.RecognitionAPI = webkitSpeechRecognition
+            this.SpeechRecognition.RecognitionEngine = new this.SpeechRecognition.RecognitionAPI()
+
+            this.SpeechRecognition.RecognitionEngine.continuous = false
+            this.SpeechRecognition.RecognitionEngine.lang = "en-US"
+            this.SpeechRecognition.RecognitionEngine.interimResults = false
+            this.SpeechRecognition.RecognitionEngine.maxAlternatives = 1
+
+            this.SpeechRecognition.Status.Ready = true
+        },
+        speechRecognise() {
+            if (
+                !this.SpeechRecognition.Status.Ready
+                || this.SpeechRecognition.Status.Listening
+                || this.SpeechRecognition.Status.Analyzing
+            ) return
             const vm = this
-            recognition.continuous = false
-            recognition.lang = "en-US"
-            recognition.interimResults = false
-            recognition.maxAlternatives = 1
 
-            recognition.start()
+            this.SpeechRecognition.RecognitionEngine.start()
+            this.SpeechRecognition.Status.Listening = true
 
-            recognition.onresult = function (event) {
-                console.log(`Result: ${event.results[0][0].transcript}`)
+            this.SpeechRecognition.RecognitionEngine.onresult = function (event) {
+                vm.SpeechRecognition.Status.Listening = false
+                vm.SpeechRecognition.Status.Analyzing = false
                 vm.titleQuery = vm.speechCapitalize(event.results[0][0].transcript)
-                vm.filterInput()
-                console.log(`Confidence: ${event.results[0][0].confidence}`)
+                vm.filter()
+                // console.log(`Confidence: ${event.results[0][0].confidence}`)
             }
 
-            recognition.onspeechend = function () {
-                recognition.stop()
-                console.log("Analyzing...")
+            this.SpeechRecognition.RecognitionEngine.onspeechend = function () {
+                vm.SpeechRecognition.Status.Listening = false
+                vm.SpeechRecognition.Status.Analyzing = true
+                vm.SpeechRecognition.RecognitionEngine.stop()
             }
 
-            recognition.onerror = function (event) {
-                console.error(`Error occurred in recognition: ${event.error}`)
+            this.SpeechRecognition.RecognitionEngine.onerror = function (event) {
+                vm.SpeechRecognition.Status.Listening = false
+                vm.SpeechRecognition.Status.Analyzing = false
+                console.error(`Error occurred in this.SpeechRecognition.RecognitionEngine: ${event.error}`)
             }
         }
     }
