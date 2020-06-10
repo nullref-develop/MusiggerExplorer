8ae0c68cf3202fa76071af0b78d94d49b585442a
Anton Zolotov <antonzolotov@outlook.com>
2020-06-10 19:24:13 +0500

    Some improvements when going to the main page by clicking on the logo. Adjusting some styles.

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,13 +1,25 @@
 <template>
-    <a
-        href="/"
+    <div
         class="logo cell"
         title="Back to the jungle"
+        @click="backToHome()"
     >
         <img
             class="logo-img"
             src="../assets/logo.svg"
             alt="Musigger"
         >
-    </a>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "Logo",
+    methods: {
+        backToHome() {
+            if (this.$route.name !== "home") this.$router.push({ name: "home", query: null })
+            else window.location.href = "/"
+        }
+    }
+}
+</script>
